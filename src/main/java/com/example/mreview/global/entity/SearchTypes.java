145255255bd04f4610d16f7501e1a1b9e655cf5e
situodys <situodys@gmail.com>
2022-08-18 23:38:11 +0900

    Refactor: 삭제 SearchTypes

--- src/main/java/com/example/mreview/global/entity/SearchTypes.java
+++
@@ -1,9 +0,0 @@
-package com.example.mreview.global.entity;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-public class SearchTypes {
-    private List<SearchValue> searchValues = new ArrayList<>();
-}
