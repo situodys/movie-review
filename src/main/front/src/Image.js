638b06cddda3ac95ad560ba823f58f321922b41c
situodys <situodys@gmail.com>
2022-08-25 00:53:07 +0900

    Chore: 추가 gradle, package.json

--- src/main/front/src/Image.js
+++
@@ -1,8 +0,0 @@
-import {API_BASE_URL} from "./config/config";
-
-function Image(props){
-    console.log(API_BASE_URL+'/display?fileName='+props.url);
-    return <img src={API_BASE_URL+'/display?fileName='+props.url}/>
-}
-
-export default Image;
--- src/main/front/src/ImageList.js
+++ src/main/front/src/ImageList.js
@@ -1,4 +1,4 @@
-import Image from "./Image";
+import Image from "./component/Image";
 import {useState} from "react";
 
 function ImageList(props) {
